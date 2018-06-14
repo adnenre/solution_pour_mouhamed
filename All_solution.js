5bf5447d295a18234ab06dd94a5bca2f7266e84a
adnen rebai <adnen.rebai@gmail.com>
2018-06-14 03:13:53 +0200

    add comment and a callback function to myOwnMap

--- All_solution.js
+++ All_solution.js
@@ -1,23 +1,55 @@
-
-
 /**
  * solution by @rebai_adnen
  */
 
- let array = [1,2,3,4];
+let array = [1, 2, 3, 4];
 
- // sum of array element
- let addElementsOfAList = ((array) => array.reduce((a,b) => a + b,0));
 
- // product off array element
- let multiplyElementsOfAList = ((array) => array.reduce((a,b) => a * b,1));
+/**
+ * 
+ * @param {Array} array of number
+ * @return {Number} return sum of all elements
+ */
+let addElementsOfAList = ((array) => array.reduce((a, b) => a + b, 0));
 
- // new array with element square
- let myOneMap= ((array) => array.reduce((a,b) => {a.push(b*2); return a;},[]));
-    
+
+
+/**
+ * @param {array}
+ * @return {Number} Returns the result of multiplication of all element
+ */
+let multiplyElementsOfAList = ((array) => array.reduce((a, b) => a * b, 1));
+
+
+
+
+/**
+ * 
+ * @param {Number} 
+ * @return {Number} return the double of a number
+ */
+let multiplyByTwo = (x) =>  x * 2;
+
+
+
+
+/**
+ * 
+ * @param {Array} array  array of number
+ * @param {function} multiplyByTwo callback function 
+ * @return {Array}  return array with double of element
+ */
+let myOneMap = ((array, multiplyByTwo) => {
+    let newArray = [];
+    newArray = array.reduce((a, b) => {
+        a.push(multiplyByTwo(b));
+        return a;
+    }, []);
+    return newArray;
+})
+
 
 
 console.log(addElementsOfAList(array));
 console.log(multiplyElementsOfAList(array));
-console.log(myOneMap(array));
-
+console.log(myOneMap(array, multiplyByTwo));
